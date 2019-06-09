db072da51a5582d56bc9b4f5081b2aab91a8da6c
Your Name <alexsigala87@gmail.com>
2019-06-09 16:52:55 -0700

    added api endpoints. fixed typos.

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,3 +1,17 @@
 require("../../server");
+require("../data/friends");
 
-app.post
+app.get("/api/friends",function(req,res){
+    console.log(__dirname);
+    //display JSON of all friends
+    res.sendFile(path.jopin(__dirname,"friends.js"));
+})
+
+app.post("/api/friends",function(req,res){
+    var newRes=req.body;
+    //push the posted json object into friends array located in friends.js
+    friends.push(newRes);
+    //respond with the friends object an array of spcific user objects
+    res.json(friends);
+    
+})
--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -4,9 +4,9 @@ require("../../server");
 //send user to the survey when they hit the sruvey route
 app.get("/survey",function(req,res){
     console.log(__dirname);
-    res.sendfile(path.join(__dirname,"survey.html"));
+    res.sendFile(path.join(__dirname,"survey.html"));
 });
 app.get("/",function(req,res){
     console.log(__dirname);
-    res.sendfile(path.join(__dirname,"home.html"))
+    res.sendFile(path.join(__dirname,"home.html"))
 })
